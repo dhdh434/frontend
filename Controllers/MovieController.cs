587586a8cbaf980c6a4c5e11dc444026096aa79c
dhdh434 <103613635+dhdh434@users.noreply.github.com>
2023-04-05 17:48:18 -0600

    Add files via upload
    bin and controllers

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mission14Api.Data;
+
+namespace mission14Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private MovieDbContext context;
+        public MovieController(MovieDbContext temp) {
+            context = temp;
+        }
+
+        //return to the json
+        public IEnumerable<Movies> Get()
+        {
+            //return the movies that are edited and order them in alphebetical order
+            return context.Movies
+                .Where(x => x.Edited == "Yes")
+                .OrderBy(x => x.Title)
+                .ToArray();
+        }
+
+    }
+}
