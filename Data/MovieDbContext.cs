70568a83faea3d6a3558bed08d4b49fc0905854b
dhdh434 <103613635+dhdh434@users.noreply.github.com>
2023-04-05 17:49:35 -0600

    Add files via upload

---
+++ Data/MovieDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace mission14Api.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options) { }
+
+        //set up a database
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
